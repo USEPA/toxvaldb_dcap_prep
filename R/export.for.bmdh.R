d0f32443f0d7fe780549191c90ee9021a40ae218
Winter <Winter.Samuel@epa.gov>
2024-05-29 17:15:01 -0400

    Update export.for.bmdh function with Tidyverse logic and new filtering/export behavior

--- R/export.for.bmdh.R
+++ R/export.for.bmdh.R
@@ -1,40 +1,46 @@
-library(openxlsx)
-library(digest)
 #-----------------------------------------------------------------------------------
-#' Export records required for calculating BMDh values.
-#'
-#' `export.for.bmdh` Exports all of the data required for the BMDh calculations.
+#' @title export.for.bmdh
+#' @description Export records required for calculating BMDh values.
+#' @param toxval.db Database version
+#' @return Write a file with the results: ToxValDB for BMDh {toxval.db} {Sys.Date()}.xlsx
+#' @details Exports all of the data required for the BMDh calculations.
 #' The main query may need to be modified to extract more columns if needed for
 #' the final application. Certain sources have been excluded because they have a high
 #' percentage of read-across values. Species are filtered to only include Human,
 #' Dog, Mouse, Rat and Rabbit. If more species are to be included, then allometric
 #' scaling factors for those need to added to the function bmd.per.study().
-#'
-#' @param toxval.db Database version
-#' @param user The username for the MySQL database. The database instance is
-#' hard-coded in the function setDBConn().
-#' @param password The user's MySQL database password.
-#' @return Write a file with the results: ToxValDB for BMDh {toxval.db} {Sys.Date()}.xlsx
 #' @export
+#' @examples
+#' \dontrun{
+#' if(interactive()){
+#'  #EXAMPLE1
+#'  }
+#' }
+#' @seealso
+#'  \code{\link[openxlsx]{createStyle}}, \code{\link[openxlsx]{write.xlsx}}
+#' @rdname export.for.bmdh
+#' @importFrom openxlsx createStyle write.xlsx
+#' @importFrom toxvaldbBMDh printCurrentFunction runQuery
+#' @importFrom dplyr distinct filter mutate case_when select
+#' @importFrom tidyr replace_na
+#' @importFrom writexl write_xlsx
 #-----------------------------------------------------------------------------------
-export.for.bmdh <- function(toxval.db="res_toxval_v95",user="_dataminer",password="pass") {
-  toxvaldbBMDh::printCurrentFunction(toxval.db)
+export.for.bmdh <- function(toxval.db="res_toxval_v95") {
+  printCurrentFunction(toxval.db)
   dir = "data/"
-  toxvaldbBMDh::setDBConn(user=user,password=password)
 
-  slist = c("ATSDR PFAS 2021","ATSDR MRLs","Copper Manufacturers",
-           "ECHA IUCLID","ECOTOX","EFSA","HAWC PFAS 430",
-            "HAWC Project","HEAST","HESS","HPVIS","IRIS",
-            "NTP PFAS","PFAS 150 SEM v2","PPRTV (CPHEA)",
-            "ToxRefDB","WHO JECFA Tox Studies")
+  slist = c("ATSDR PFAS 2021", "ATSDR MRLs", "Cal OEHHA", "Copper Manufacturers",
+           "ECHA IUCLID", "ECOTOX", "EFSA", "HAWC PFAS 430",
+            "HAWC Project", "Health Canada", "HESS", "HPVIS", "IRIS",
+            "NTP PFAS", "PPRTV (CPHEA)", "ToxRefDB","WHO JECFA Tox Studies")
 
-  #slist = "ECOTOX"
+  # Get priority values for each specified source
   plist = vector(mode="integer",length=length(slist))
   plist[] = 1
-  for(i in 1:length(slist)) {
+  for(i in seq_len(length(slist))) {
     src = slist[i]
     query = paste0("select distinct priority from record_source where source='",src,"' and long_ref!='-'")
-    vals = toxvaldbBMDh::runQuery(query,toxval.db)[,1]
+    vals = runQuery(query,toxval.db)[,1]
     cat(src,paste(vals,collapse="|"),"\n")
     if(length(vals)>0) plist[i] = vals[1]
     else {
@@ -43,74 +49,96 @@ export.for.bmdh <- function(toxval.db="res_toxval_v95",user="_dataminer",passwor
       if(src=="ECHA IUCLID") plist[i] = 1
     }
   }
+
+  # Query ToxVal for source data
   res = NULL
-  for(i in 1:length(slist)) {
+  for(i in seq_len(length(slist))) {
     src = slist[i]
     priority = plist[i]
-    query = paste0("SELECT
-                      a.dtxsid,a.casrn,a.name,
-                      b.source,
-                      b.toxval_type,
-                      b.toxval_subtype,
-                      b.toxval_numeric_qualifier,
-                      b.toxval_numeric,
-                      b.toxval_units,
-                      b.study_type,
-                      b.study_duration_value,
-                      b.study_duration_units,
-                      b.study_duration_class,
-                      d.common_name,
-                      b.strain,
-                      b.sex,
-                      b.lifestage,
-                      b.generation,
-                      b.exposure_route,
-                      b.exposure_method,
-                      b.exposure_form,
-                      b.critical_effect,
-                      b.year,
-                      f.long_ref,
-                      f.url,
-                      f.title,
-                      f.pmid,
-                      f.guideline,
-                      f.record_source_level,
-                      f.record_source_type,
-                      f.priority,
-                      f.clowder_doc_id,
-                      b.source_hash,
-                      b.study_group,
-                      b.qc_status,
-                      b.experimental_record,
-                      a.cleaned_casrn,a.cleaned_name
-                      FROM
-                      toxval b
-                      INNER JOIN source_chemical a on a.chemical_id=b.chemical_id
-                      LEFT JOIN species d on b.species_id=d.species_id
-                      INNER JOIN toxval_type_dictionary e on b.toxval_type=e.toxval_type
-                      INNER JOIN record_source f on b.toxval_id=f.toxval_id
-                      WHERE
-                      b.source='",src,"'
-                      #!and b.human_eco='human health'
-                      and e.toxval_type_supercategory in ('Point of Departure')
-                      and b.toxval_units='mg/kg-day'
-                      and b.exposure_route='oral'
-                      #!and f.priority=",priority,"
-                     ")
 
+    # Handle inclusion of only specified IUCLID OHTs
+    iuclid_addition = NULL
+    if(src == "ECHA IUCLID") {
+      iuclid_addition = paste0(" AND b.source_table in ",
+                               "('source_iuclid_repeateddosetoxicityoral', ",
+                                "'source_iuclid_developmentaltoxicityteratogenicity', ",
+                                "'source_iuclid_carcinogenicity', ",
+                                "'source_iuclid_immunotoxicity', ",
+                                "'source_iuclid_neurotoxicity')")
+    }
 
-    mat = toxvaldbBMDh::runQuery(query,toxval.db,T,F)
-    mat = unique(mat)
+    query = paste0("SELECT ",
+                      "a.dtxsid, a.casrn, a.name, ",
+                      "b.source, ",
+                      "b.toxval_type, ",
+                      "b.toxval_subtype, ",
+                      "b.toxval_numeric_qualifier, ",
+                      "b.toxval_numeric, ",
+                      "b.toxval_units, ",
+                      "b.study_type, ",
+                      "b.study_duration_value, ",
+                      "b.study_duration_units, ",
+                      "b.study_duration_class, ",
+                      "d.common_name, ",
+                      "b.strain, ",
+                      "b.sex, ",
+                      "b.lifestage, ",
+                      "b.generation, ",
+                      "b.exposure_route, ",
+                      "b.exposure_method, ",
+                      "b.exposure_form, ",
+                      "b.critical_effect, ",
+                      "b.year, ",
+                      "f.long_ref, ",
+                      "f.url, ",
+                      "f.title, ",
+                      "f.pmid, ",
+                      "f.guideline, ",
+                      "f.record_source_level, ",
+                      "f.record_source_type, ",
+                      "f.priority, ",
+                      "f.clowder_doc_id, ",
+                      "b.source_hash, ",
+                      "b.study_group, ",
+                      "b.qc_status, ",
+                      "b.experimental_record, ",
+                      "a.cleaned_casrn, a.cleaned_name ",
+                      "FROM ",
+                      "toxval b ",
+                      "INNER JOIN source_chemical a on a.chemical_id=b.chemical_id ",
+                      "LEFT JOIN species d on b.species_id=d.species_id ",
+                      "INNER JOIN toxval_type_dictionary e on b.toxval_type=e.toxval_type ",
+                      "INNER JOIN record_source f on b.toxval_id=f.toxval_id ",
+                      "WHERE ",
+                      "b.source='", src, "' ",
+                      # "and b.human_eco='human health' ",
+                      "and e.toxval_type_supercategory in ('Point of Departure') ",
+                      "and b.toxval_units='mg/kg-day' ",
+                      "and b.exposure_route='oral'",
+                      # " and f.priority='", priority, "'",
+                      iuclid_addition
+                     )
+
+    # Get unique entries using query
+    mat = runQuery(query, toxval.db) %>%
+      dplyr::distinct()
     cat("[1]",src,":",nrow(mat),"\n")
-    mat[is.na(mat$toxval_numeric_qualifier),"toxval_numeric_qualifier"] = "ns"
-    #mat[is.element(mat$toxval_numeric_qualifier,c("~","<","<=")),"toxval_numeric_qualifier"] = "="
-    #mat = mat[mat$toxval_numeric_qualifier=="=",]
 
+    # Initialize list of toxval_type values to exclude
     exclude.list = c("HNEL","LEC","NOTEL","POD (HE)","POD (HEC)","POD (surrogate)","critical value",
                      "LOEC","NOEC","LOAEC","NOAEC","BMD (HEC)","BMDL (HEC)","POD (01 HED)","POD (HED)","LED","POD (99 HED)")
-    mat = mat[!is.element(mat$toxval_type,exclude.list),]
+    mat = mat %>%
+      # Remove entries with excluded toxval_type
+      dplyr::filter(!toxval_type %in% !!exclude.list) %>%
+
+      # Replace NA qualifier with "ns"
+      dplyr::mutate(
+        toxval_numeric_qualifier = toxval_numeric_qualifier %>%
+          tidyr::replace_na("ns")
+      )
     cat("[2]",src,":",nrow(mat),"\n")
 
+    # Initialize list of study_type values to include
     stlist = c(
       "short-term",
       "subchronic",
@@ -132,27 +160,59 @@ export.for.bmdh <- function(toxval.db="res_toxval_v95",user="_dataminer",passwor
       "neurotoxicity short-term",
       "neurotoxicity subchronic"
      )
-
-    if(!is.element(src,c("HEAST"))) mat = mat[is.element(mat$study_type,stlist),]
+    # Remove entries with invalid study_types
+    if(src != "HEAST") mat = mat %>% dplyr::filter(study_type %in% !!stlist)
     cat("[3]",src,":",nrow(mat),"\n")
 
-    mat[mat$common_name=="European Rabbit","common_name"] = "Rabbit"
-    mat[mat$common_name=="Norway Rat","common_name"] = "Rat"
-    mat[mat$common_name=="Domestic Dog","common_name"] = "Dog"
-    mat[mat$common_name=="House Mouse","common_name"] = "Mouse"
-    #mat[mat$common_name=="Rabbit Family","common_name"] = "Rabbit"
-    mat = mat[is.element(mat$common_name,c("Rat","Mouse","Dog","Rabbit","Human")),]
+    mat = mat %>%
+      # Clean common_name values
+      dplyr::mutate(
+        common_name = dplyr::case_when(
+          common_name == "European Rabbit" ~ "Rabbit",
+          common_name == "Norway Rat" ~ "Rat",
+          common_name == "Domestic Dog" ~ "Dog",
+          common_name == "House Mouse" ~ "Mouse",
+          TRUE ~ common_name
+        ),
 
-    mat[is.na(mat$casrn),"casrn"] = mat[is.na(mat$casrn),"cleaned_casrn"]
-    mat[mat$casrn=='-',"casrn"] = mat[mat$casrn=='-',"cleaned_casrn"]
-    mat[is.na(mat$name),"name"] = mat[is.na(mat$name),"cleaned_name"]
-    mat[mat$name=='-',"name"] = mat[mat$name=='-',"cleaned_name"]
-    cremove = c("cleaned_name","cleaned_casrn")
-    mat = mat[ , !(names(mat) %in% cremove)]
+        # Use cleaned name and casrn values when original values are missing
+        casrn = dplyr::case_when(
+          casrn %in% c(as.character(NA), "-", "") ~ cleaned_casrn,
+          TRUE ~ casrn
+        ),
+        name = dplyr::case_when(
+          name %in% c(as.character(NA), "-", "") ~ cleaned_name,
+          TRUE ~ name
+        ),
+
+        # Add toxval_numeric_hed flag field
+        toxval_numeric_hed = dplyr::case_when(
+          grepl("HED", toxval_type) ~ 1,
+          TRUE ~ 0
+        )
+      ) %>%
+      # Keep only entries with specified common_name
+      dplyr::filter(common_name %in% c("Rat", "Mouse", "Dog", "Rabbit", "Human")) %>%
+      # Remove unused "cleaned" columns
+      dplyr::select(-c("cleaned_casrn", "cleaned_name")) %>%
+      # Remove non-experimental records
+      dplyr::filter(!experimental_record %in% c("no", "No", "not experimental", "Not experimental"))
+
+
     cat("[4]",src,":",nrow(mat),"\n\n")
+
+    # Add current source data to running total
     res = rbind(res,mat)
   }
-  sty = openxlsx::createStyle(halign="center",valign="center",textRotation=90,textDecoration = "bold")
+
+  # Write unique toxval_type values included in full data
+  unique_toxval_type = res %>%
+    dplyr::select(source, toxval_type) %>%
+    dplyr::distinct()
+  file = paste0(dir,"results/bmdh_export_toxval_type_",Sys.Date(),".xlsx")
+  writexl::write_xlsx(unique_toxval_type, file)
+
+  # Write full data to file
   file = paste0(dir,"results/ToxValDB for BMDh ",toxval.db," ",Sys.Date(),".xlsx")
-  openxlsx::write.xlsx(res,file,firstRow=T,headerStyle=sty)
+  writexl::write_xlsx(res, file)
 }
