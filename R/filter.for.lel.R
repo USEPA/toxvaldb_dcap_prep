9a1e658996131963b551b25427098d14eba732c7
Winter <Winter.Samuel@epa.gov>
2024-06-11 14:47:43 -0400

    Initial efficiency fixes for slowest functions

--- R/filter.for.lel.R
+++ R/filter.for.lel.R
@@ -22,28 +22,40 @@ filter.for.lel <- function(toxval.db="res_toxval_v95",sys.date=Sys.Date()) {
   dir = "data/"
   file = paste0(dir,"results/ToxValDB for BMDh ",toxval.db," ",sys.date,".xlsx")
   print(file)
-  res = openxlsx::read.xlsx(file)
-  t1 = res[is.element(res$toxval_type,c("NEL","LEL","LOEL","NOEL")),]
+  res = readxl::read_xlsx(file)
+  sgin = res %>%
+    dplyr::filter(toxval_type %in% c("NEL","LEL","LOEL","NOEL")) %>%
+    dplyr::pull(study_group) %>%
+    unique()
 
-  sgin = unique(t1$study_group)
-  t2a = res[is.element(res$study_group,sgin),]
-  t2b = res[!is.element(res$study_group,sgin),]
-
+  t2a = res %>%
+    dplyr::filter(study_group %in% sgin)
+  t2b = res %>%
+    dplyr::filter(!(study_group %in% sgin))
   t2c = NULL
+
   for(sg in sgin) {
-    t3 = t2a[is.element(t2a$study_group,sg),]
-    ttlist = unique(t3$toxval_type)
-    #cat(t3[1,"source"],":",paste(ttlist,collapse="|"),"\n")
-    if(is.element("LOAEL",ttlist) || is.element("NOAEL",ttlist)) {
-      if(is.element("LOAEL",ttlist)) t3 = t3[!is.element(t3$toxval_type,c("LEL","LOEL")),]
-      if(is.element("NOAEL",ttlist)) t3 = t3[!is.element(t3$toxval_type,c("NEL","NOEL")),]
-      t2c = rbind(t2c,t3)
+    t3 = t2a %>%
+      dplyr::filter(study_group == !!sg)
+    ttlist = t3 %>%
+      dplyr::pull(toxval_type) %>%
+      unique()
+
+    if(is.element("LOAEL",ttlist) || is.element("NOAEL",ttlist)) t2c = dplyr::bind_rows(t2c, t3)
+    else {
+      if(is.element("LOAEL",ttlist)) {
+        t2c = t2c %>%
+          dplyr::bind_rows(t3 %>% dplyr::filter(!(toxval_type %in% c("LEL","LOEL"))))
+      }
+      if(is.element("NOAEL",ttlist)) {
+        t2c = t2c %>%
+          dplyr::bind_rows(t3 %>% dplyr::filter(!(toxval_type %in% c("NEL","NOEL"))))
+      }
     }
-    else t2c = rbind(t2c,t3)
   }
 
-  t4 = rbind(t2b,t2c)
+  t4 = dplyr::bind_rows(t2b, t2c)
+
   file = paste0(dir,"results/ToxValDB for BMDh LEL NEL filtered ",toxval.db," ",sys.date,".xlsx")
-  sty = openxlsx::createStyle(halign="center",valign="center",textRotation=90,textDecoration = "bold")
-  openxlsx::write.xlsx(t4,file,firstRow=T,headerStyle=sty)
+  writexl::write_xlsx(t4, file)
 }
