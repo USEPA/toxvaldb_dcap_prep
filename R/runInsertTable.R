d8c68974d85dd6e8710616e9ad86b2d7d35c3e87
Winter <Winter.Samuel@epa.gov>
2024-07-18 16:47:29 -0400

    Updated toxvaldbBMDh workflow to use Renviron for queries

--- R/runInsertTable.R
+++ R/runInsertTable.R
@@ -1,39 +1,41 @@
-#--------------------------------------------------------------------------------------
+#' @description Inserts multiple rows into a database table
+#'
 #' @param mat data frame containing the data, with the column names corresponding
 #' @param table name of the database table to which data will be inserted
 #' @param db the name of the database
 #' @param do.halt if TRUE, halt on errors or warnings
 #' @param verbose if TRUE, print diagnostic information
+#' @param get.id Whether to return ID or not, Default: T
 #' @title runInsertTable
-#' @description Inserts multiple rows into a database table
-#' @param get.id PARAM_DESCRIPTION, Default: T
-#' @return None; inserts data into DB
+#' @return ID or None
 #' @details DETAILS
-#' @examples 
+#' @examples
 #' \dontrun{
 #' if(interactive()){
 #'  #EXAMPLE1
 #'  }
 #' }
-#' @seealso 
+#' @seealso
 #'  \code{\link[RMySQL]{character(0)}}, \code{\link[RMySQL]{MySQLDriver-class}}
 #' @rdname runInsertTable
-#' @export 
+#' @export
 #' @importFrom RMySQL dbConnect MySQL dbWriteTable dbSendQuery dbFetch dbHasCompleted dbClearResult dbDisconnect
 #--------------------------------------------------------------------------------------
-runInsertTable <- function(mat,table,db,do.halt=T,verbose=F,get.id=T) {
-  if(!exists("DB.SERVER")) {
-    cat("DB.SERVER not defined\n")
+runInsertTable <- function(mat, table, db, do.halt=TRUE, verbose=FALSE, get.id=TRUE) {
+  if(is.null(table)){
+    cat("No table provided...\n")
     return(NULL)
   }
-  if(!exists("DB.USER")) {
-    cat("DB.USER not defined\n")
-    return(NULL)
+
+  # Check environment variables for database credentials are set
+  credentials = c("db_user", "db_pass", "db_server", "db_port")
+  for(cred in credentials){
+    if(Sys.getenv(cred) == ""){
+      cat(paste0("'", cred, "' environment variable not defined\n"))
+      return(NULL)
+    }
   }
-  if(!exists("DB.PASSWORD")) {
-    cat("DB.PASSWORD not defined\n")
-    return(NULL)
-  }
+
   if(verbose) {
     printCurrentFunction()
     cat("mat: ",dim(mat),"\n")
@@ -41,29 +43,14 @@ runInsertTable <- function(mat,table,db,do.halt=T,verbose=F,get.id=T) {
     cat("db: ",db,"\n")
   }
   tryCatch({
-    #
-    # make sure all charactersc are in UTF8
-    #
-    desc <- runQuery(paste0("desc ",table),db)
-    desc <- desc[is.element(desc[,"Field"],names(mat)),]
-    for(i in seq_len(dim(desc)[1])) {
-      col <- desc[i,"Field"]
-      type <- desc[i,"Type"]
-      if(grepl("varchar|text", type)) {
-        if(verbose) cat("   enc2utf8:",col,"\n")
-        x <- as.character(mat[,col])
-        x[is.na(x)] <- "-"
-        x <- enc2native(x)
-        x <- iconv(x,from="latin1",to="UTF-8")
-        x <- iconv(x,from="LATIN1",to="UTF-8")
-        x <- iconv(x,from="LATIN2",to="UTF-8")
-        x <- iconv(x,from="latin-9",to="UTF-8")
-        mat[,col] <- enc2utf8(x)
-      }
-    }
-    if(DB.PORT<0) con <- RMySQL::dbConnect(drv=RMySQL::MySQL(),user=DB.USER,password=DB.PASSWORD,host=DB.SERVER,dbname=db)
-    else con <- RMySQL::dbConnect(drv=RMySQL::MySQL(),user=DB.USER,password=DB.PASSWORD,host=DB.SERVER,port=DB.PORT,dbname=db)
-    ret <- RMySQL::dbWriteTable(con,name=table,value=mat,row.names=F,overwrite=F,append=T)
+    con <- RMySQL::dbConnect(drv=RMySQL::MySQL(),
+                             user=Sys.getenv("db_user"),
+                             password=Sys.getenv("db_pass"),
+                             host=Sys.getenv("db_server"),
+                             dbname=db,
+                             port=as.numeric(Sys.getenv("db_port"))
+    )
+    res = RMySQL::dbWriteTable(con,name=table,value=mat,row.names=FALSE, overwrite=FALSE, append=TRUE)
     if(get.id) {
       rs2 <- RMySQL::dbSendQuery(con, "select LAST_INSERT_ID()")
       d2 <- RMySQL::dbFetch(rs2, n = -1)
